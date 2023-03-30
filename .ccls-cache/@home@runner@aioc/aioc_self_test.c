b1733f2e8bee05eadc788ea8535505d0c4df7699
rshaheni <>
2023-03-30 18:41:39 +0000

    Self test.

--- .ccls-cache/@home@runner@aioc/aioc_self_test.c
+++ .ccls-cache/@home@runner@aioc/aioc_self_test.c
@@ -2,6 +2,19 @@
 //==============================================================================
 #include "aioc_api.h"
 
+
+typedef struct
+{
+  aioc_analog_id_t ai_id;
+  uint16_t upper_limit;
+  uint16_t lower_limit;
+}
+aioc_test_table_entry_t;
+
+static aioc_test_table_entry_t bit_high_test_table[] =
+// TBD
+{0};
+
 aioc_error_t aioc_self_test(void)
 {
   aioc_error_t e = error_none;
@@ -13,24 +26,36 @@ aioc_error_t aioc_self_test(void)
   {
     return e;
   }
-  
-  // Stimulate the signal under test.
-  // TBD
 
-   
-  e = aioc_api_single_channel_conversion(
-        AIOC_AI_LEFT_FWD_OVER_PRESSURE_SENSOR,
-        &result);
-  if (e)
-  {
-    return e;
-  }
+  // Test the BIT HIGH input selections.
+  {    
+    aioc_test_table_entry_t* test_table = bit_high_test_table;
+    uint32_t test_table_length =
+                sizeof(bit_high_test_table)/
+                sizeof(*bit_high_test_table);
+    uint32_t i = 0;
+    
+    // Multiplex in the BIT High inputs.
+    // TBD
   
-  //Check the results.
-  // TBD
-  
+    // Process list of analog inputs and verify the results for each input.
+    for (i = 0; i < test_table_length; i++)
+    {
+      aioc_analog_id_t ai_id = test_table[i].ai_id;
+      uint16_t upper_limit = test_table[i].upper_limit;
+      uint16_t lower_limit = test_table[i].lower_limit;
+      
+    
+      e = aioc_api_single_channel_conversion(ai_id, &result);
+      if (e)
+      {
+        return e;
+      }
+    }
+
+    //Check the results.
+    // TBD
+  }  
   return error_none;
 }
-
-
 
