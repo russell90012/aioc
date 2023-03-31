09de86a1e56d1eada83d7531e546072cfecb7721
rshaheni <>
2023-03-31 18:51:41 +0000

    Updated incremented self test.

--- aioc_self_test.c
+++ aioc_self_test.c
@@ -1,5 +1,8 @@
 //==============================================================================
 //==============================================================================
+#include <stdio.h>
+
+
 #include "aioc_api.h"
 
 #include "aioc_mux.h"
@@ -28,13 +31,27 @@ static aioc_test_table_entry_t bit_high_test_table[] =
   {AIOC_AI_FWD_RIGHT_FEEDPIPE_PRESSURE_SENSOR, 0, 0}
 };
 
+static char* aioc_ai_names[] =
+{
+  "AIOC_AI_LEFT_FWD_OVER_PRESSURE_SENSOR",
+  "AIOC_AI_LEFT_AFT_OVER_PRESSURE_SENSOR"
+  "AIOC_AI_RIGHT_FWD_OVER_PRESSURE_SENSOR",
+  "AIOC_AI_RIGHT_AFT_OVER_PRESSURE_SENSOR",
+  "AIOC_AI_AFT_RIGHT_FEEDPIPE_PRESSURE_SENSOR",
+  "AIOC_AI_FWD_LEFT_FEEDPIPE_PRESSURE_SENSOR",
+  "AIOC_AI_AFT_LEFT_FEEDPIPE_PRESSURE_SENSOR",
+  "AIOC_AI_FWD_RIGHT_FEEDPIPE_PRESSURE_SENSOR"
+};
+
 //==============================================================================
 //==============================================================================
 aioc_error_t aioc_self_test(void)
 {
   aioc_error_t e = error_none;
   uint16_t result;
- 
+
+  printf("%s: initializing aioc_api.\n", __FUNCTION__);
+  
   // Initialize the board devices.
   e = aioc_api_init();
   if (e)
@@ -42,6 +59,7 @@ aioc_error_t aioc_self_test(void)
     return e;
   }
 
+  printf("%s: testing the BIT HIGH input selections.\n", __FUNCTION__);
   // Test the BIT HIGH input selections.
   {    
     aioc_test_table_entry_t* test_table = bit_high_test_table;
@@ -50,6 +68,8 @@ aioc_error_t aioc_self_test(void)
                 sizeof(*bit_high_test_table);
     uint32_t i = 0;
     
+    printf("%s: switching in the BIT HIGH inputs.\n", __FUNCTION__);
+    
     // Multiplex in the BIT High inputs for 5V adc.
     e = aioc_mux_switch_lines(AIOC_MUX_LINES_BIT_HIGH, AIOC_MUX_BANKS_5V);
     if (e)
@@ -57,6 +77,8 @@ aioc_error_t aioc_self_test(void)
       return e;
     }
   
+    printf("%s: processing the list of analog inputs.\n", __FUNCTION__);
+
     // Process list of analog inputs and verify the results for each input.
     for (i = 0; i < test_table_length; i++)
     {
@@ -64,15 +86,24 @@ aioc_error_t aioc_self_test(void)
       uint16_t upper_limit = test_table[i].upper_limit;
       uint16_t lower_limit = test_table[i].lower_limit;
       
+      printf("%s: processing analog input: %s\n", __FUNCTION__, aioc_ai_names[ai_id]);
       e = aioc_api_single_channel_conversion(ai_id, &result);
       if (e)
       {
         return e;
       }
-    }
+      printf("%s: checking result %x, upper limit %x, lower limit %x\n", __FUNCTION__,
+        result, upper_limit, lower_limit);
 
-    //Check the results.
-    // TBD
+      if (result < upper_limit || result > lower_limit)
+      {
+        printf("%s: result out of range, exiting.\n");
+        break;
+      }
+      printf("%s: result in range, continuing\n");      
+      
+    }
+    printf("%s: done.\n", __FUNCTION__);
   }  
   return error_none;
 }
--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
+  aioc_self_test();
   return 0;
 }
